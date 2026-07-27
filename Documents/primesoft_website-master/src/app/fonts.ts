b310f1446e26b2b65d075d108172cbfa06346105
Mr Aziz Msami <azizomary99@gmail.com>
2026-07-27 15:19:12 +0300

    Remove duplicate nested project folder

--- Documents/primesoft_website-master/src/app/fonts.ts
+++
@@ -1,8 +0,0 @@
-// app/fonts.ts
-import localFont from "next/font/local";
-
-export const myCustomFont = localFont({
-  src: "../../public/fonts/odoo-font.woff2", 
-  display: "swap",
-  variable: "--font-custom",
-});
